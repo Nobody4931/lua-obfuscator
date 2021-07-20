bc23ede85a956db9133974cc63ed1e3c274898a3
Nobody4931 <ardenxie1@gmail.com>
2021-07-20 19:47:19 -0400

    please shut up compiler

--- lib/opcodes/vopcode.hpp
+++ lib/opcodes/vopcode.hpp
@@ -10,11 +10,13 @@ class vopcode_t {
 public:
 	virtual bool valid( instruction_t& instruction ) = 0;
 	virtual std::string string() = 0;
+	virtual ~vopcode_t() { };
 };
 
 class vmutator_t {
 public:
 	virtual vopcode_t* mutate() = 0;
+	virtual ~vmutator_t() { };
 };
 
 #endif
