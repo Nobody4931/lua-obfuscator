93ccb5b855740a2d7a2a69b39ae7accca20f3750
Nobody4931 <ardenxie1@gmail.com>
2021-07-22 17:46:37 -0400

    op_setglobal

---
+++ lib/opcodes/op_setglobal.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#ifndef SETGLOBAL_HEADER_HPP
+#define SETGLOBAL_HEADER_HPP
+
+#include <random>
+#include <string>
+
+#include "bytecode/instruction.hpp"
+#include "opcodes/vopcode.hpp"
+
+class vop_setglobal_t : public vopcode_t {
+public:
+	bool valid( instruction_t& instruction ) override {
+		return instruction.opcode == OP_SETGLOBAL;
+	}
+
+	std::string string() override {
+		return "Env[Constants[Instr[2]]] = Stack[Instr[1]]";
+	}
+};
+
+class vmut_setglobal_t : public vmutator_t {
+public:
+	vopcode_t* mutate( std::default_random_engine& rand_engine ) override {
+		return new vop_setglobal_t();
+	}
+};
+
+
+#endif
