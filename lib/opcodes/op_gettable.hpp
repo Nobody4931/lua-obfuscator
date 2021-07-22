925649f0ba4f61ef11d8a609f25725aaf0ca185c
Nobody4931 <ardenxie1@gmail.com>
2021-07-21 20:25:41 -0400

    op_gettable probably broken bc my lsp stopped working when i was coding it lol

---
+++ lib/opcodes/op_gettable.hpp
@@ -0,0 +1,75 @@
+#pragma once
+#ifndef OP_GETTABLE_HEADER_HPP
+#define OP_GETTABLE_HEADER_HPP
+
+#include <string>
+#include <random>
+
+#include "bytecode/instruction.hpp"
+#include "opcodes/vopcode.hpp"
+
+class vop_gettable_1_t : public vopcode_t {
+public:
+	bool valid( instruction_t& instruction ) override {
+		return instruction.opcode == OP_GETTABLE;
+	}
+
+	std::string string() override {
+		return R"(local Key
+		if Instr[3] > 255 then
+			Key = Constants[Instr[3] - 256]
+		else
+			Key = Stack[Instr[3]]
+		end
+		Stack[Instr[1]] = Stack[Instr[2]][Key])";
+	}
+};
+
+class vop_gettable_2_t : public vopcode_t {
+public:
+	bool valid( instruction_t& instruction ) override {
+		return instruction.opcode == OP_GETTABLE;
+	}
+
+	std::string string() override {
+		return "Stack[Instr[1]] = Stack[Instr[2]][ Instr[3] <= 255 and Stack[Instr[3]] or Constants[Instr[3] - 256] ]";
+	}
+};
+
+class vop_gettable_c1_t : public vopcode_t {
+public:
+	bool valid( instruction_t& instruction ) override {
+		return instruction.opcode == OP_GETTABLE && instruction.c > 255;
+	}
+
+	std::string string() override {
+		return "Stack[Instr[1]] = Stack[Instr[2]][Constants[Instr[3] - 256]]";
+	}
+}
+
+class vop_gettable_c2_t : public vopcode_t {
+public:
+	bool valid( instruction_t& instruction ) override {
+		return instruction.opcode == OP_GETTABLE && instruction.c <= 255;
+	}
+
+	std::string string() override {
+		return "Stack[Instr[1]] = Stack[Instr[2]][Stack[Instr[3]]]";
+	}
+}
+
+class vmut_gettable_t : public vmutator_t {
+public:
+	vopcode_t* mutate( std::default_random_engine& rand_engine ) override {
+		switch ( rand_engine() % 4 ) {
+			case 0: return new vop_gettable_1_t();
+			case 1: return new vop_gettable_2_t();
+			case 2: return new vop_gettable_c1_t();
+			case 3: return new vop_gettable_c2_t();
+		}
+
+		return nullptr;
+	}
+};
+
+#endif
