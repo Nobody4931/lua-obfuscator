b3577fb5faca3de972fceacfd33513d5bada52d7
Nobody4931 <ardenxie1@gmail.com>
2021-07-23 11:47:19 -0400

    op_mul

---
+++ lib/opcodes/op_mul.hpp
@@ -0,0 +1,96 @@
+#pragma once
+#ifndef OP_MUL_HEADER_HPP
+#define OP_MUL_HEADER_HPP
+
+#include <random>
+#include <string>
+
+#include "bytecode/instruction.hpp"
+#include "opcodes/vopcode.hpp"
+
+class vop_mul_t : public vopcode_t {
+public:
+	bool valid( instruction_t& instruction ) override {
+		return instruction.opcode == OP_MUL;
+	}
+
+	std::string string() override {
+		return R"(local KeyB, KeyC = function(Self, Field)
+			return Self, Instr[Field] > 255 and Constants[Instr[Field] - 256] or Stack[Instr[Field]]
+		end
+		KeyB, KeyC = KeyB(KeyB, 3)
+		Stack[Instr[1]] = KeyC * KeyB(1, 2))";
+	}
+};
+
+class vop_mul_bc_1_t : public vopcode_t {
+public:
+	bool valid( instruction_t& instruction ) override {
+		return instruction.opcode == OP_MUL &&
+			instruction.b <= 255 &&
+			instruction.c <= 255;
+	}
+
+	std::string string() override {
+		return "Stack[Instr[1]] = Stack[Instr[3]] * Stack[Instr[2]]";
+	}
+};
+
+class vop_mul_bc_2_t : public vopcode_t {
+public:
+	bool valid( instruction_t& instruction ) override {
+		return instruction.opcode == OP_MUL &&
+			instruction.b > 255 &&
+			instruction.c <= 255;
+	}
+
+	std::string string() override {
+		return "Stack[Instr[1]] = -Constants[Instr[2] - 256] * -Stack[Instr[3]]";
+	}
+};
+
+class vop_mul_bc_3_t : public vopcode_t {
+public:
+	bool valid( instruction_t& instruction ) override {
+		return instruction.opcode == OP_MUL &&
+			instruction.b <= 255 &&
+			instruction.c > 255;
+	}
+
+	std::string string() override {
+		return R"(Stack[Instr[1]] = Stack[Instr[2]] * -Constants[Instr[3] - 256]
+		Stack[Instr[1]] = -Stack[Instr[1]])";
+	}
+};
+
+class vop_mul_bc_4_t : public vopcode_t {
+public:
+	bool valid( instruction_t& instruction ) override {
+		return instruction.opcode == OP_MUL &&
+			instruction.b > 255 &&
+			instruction.c > 255;
+	}
+
+	std::string string() override {
+		return R"(Stack[Instr[1]] = -1
+		local Result = Constants[Instr[3] - 256] * -Constants[Instr[2] - 256]
+		Stack[Instr[1]] = Result * Stack[Instr[1]])";
+	}
+};
+
+class vmut_mul_t : public vmutator_t {
+public:
+	vopcode_t* mutate( std::default_random_engine& rand_engine ) override {
+		switch ( rand_engine() % 5 ) {
+			case 0: return new vop_mul_t();
+			case 1: return new vop_mul_bc_1_t();
+			case 2: return new vop_mul_bc_2_t();
+			case 3: return new vop_mul_bc_3_t();
+			case 4: return new vop_mul_bc_4_t();
+		}
+
+		return nullptr;
+	}
+};
+
+#endif
