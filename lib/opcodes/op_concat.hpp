197c87076a1b6fda9457002e50dd154e27ee8974
Nobody4931 <ardenxie1@gmail.com>
2021-07-23 20:55:05 -0400

    op_concat

---
+++ lib/opcodes/op_concat.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#ifndef OP_CONCAT_HEADER_HPP
+#define OP_CONCAT_HEADER_HPP
+
+#include <random>
+#include <string>
+
+#include "bytecode/instruction.hpp"
+#include "opcodes/vopcode.hpp"
+
+class vop_concat_t : public vopcode_t {
+	bool valid( instruction_t &instruction ) override {
+		return instruction.opcode == OP_CONCAT;
+	}
+
+	std::string string() override {
+		return R"(local Offset = Instr[2] + 1
+		local Result = Stack[Offset - 1]
+		for Idx = Offset, Instr[3] do
+			Result = Result .. Stack[Idx]
+		end
+		Stack[Instr[1]] = Result)";
+	}
+};
+
+class vmut_concat_t : public vmutator_t {
+	vopcode_t* mutate( std::default_random_engine &rand_engine ) override {
+		return new vop_concat_t();
+	}
+};
+
+#endif
