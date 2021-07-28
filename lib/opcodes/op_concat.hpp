1d88e182afbd6bed5a30585f66ac6fb8cc448368
Nobody4931 <ardenxie1@gmail.com>
2021-07-28 14:28:47 -0400

    fixing opcodes pt 3

--- lib/opcodes/op_concat.hpp
+++ lib/opcodes/op_concat.hpp
@@ -8,7 +8,22 @@
 #include "bytecode/instruction.hpp"
 #include "opcodes/vopcode.hpp"
 
-class vop_concat_t : public vopcode_t {
+class vop_concat_1_t : public vopcode_t {
+public:
+	bool valid( instruction_t& instruction ) override {
+		return instruction.opcode == OP_CONCAT;
+	}
+
+	std::string string() override {
+		return R"(local Result = Stack[Instr[2]]
+		for Idx = Instr[2] + 1, Instr[3] do
+			Result = Result .. Stack[Idx]
+		end
+		Stack[Instr[1]] = Result)";
+	}
+};
+
+class vop_concat_2_t : public vopcode_t {
 public:
 	bool valid( instruction_t& instruction ) override {
 		return instruction.opcode == OP_CONCAT;
@@ -17,17 +32,79 @@ public:
 	std::string string() override {
 		return R"(local Offset = Instr[2] + 1
 		local Result = Stack[Offset - 1]
-		for Idx = Offset, Instr[3] do
-			Result = Result .. Stack[Idx]
+		while Offset <= Instr[3] do
+			Result = Result .. Stack[Offset]
+			Offset = Offset + 1
 		end
 		Stack[Instr[1]] = Result)";
+	}
+};
+
+class vop_concat_3_t : public vopcode_t {
+public:
+	bool valid( instruction_t& instruction ) override {
+		return instruction.opcode == OP_CONCAT;
+	}
+
+	std::string string() override {
+		return R"(Stack[Instr[1]] = Stack[Instr[2]]
+		local Offset = Instr[2]
+		repeat
+			Offset = Offset + 1
+			Stack[Instr[1]] = Stack[Instr[1]] .. Stack[Offset]
+		until Offset == Instr[3])";
+	}
+};
+
+class vop_concat_bc_1_t : public vopcode_t {
+public:
+	bool valid( instruction_t& instruction ) override {
+		return instruction.opcode == OP_CONCAT &&
+			instruction.b + 1 == instruction.c;
+	}
+
+	std::string string() override {
+		return "Stack[Instr[1]] = Stack[Instr[2]] .. Stack[Instr[3]]";
+	}
+};
+
+class vop_concat_bc_2_t : public vopcode_t {
+public:
+	bool valid( instruction_t& instruction ) override {
+		return instruction.opcode == OP_CONCAT &&
+			instruction.b + 1 == instruction.c;
+	}
+
+	std::string string() override {
+		return "Stack[Instr[1]] = Stack[Instr[2]] .. Stack[Instr[2] + 1]";
+	}
+};
+
+class vop_concat_bc_3_t : public vopcode_t {
+public:
+	bool valid( instruction_t& instruction ) override {
+		return instruction.opcode == OP_CONCAT &&
+			instruction.b + 1 == instruction.c;
+	}
+
+	std::string string() override {
+		return "Stack[Instr[1]] = Stack[Instr[3] - 1] .. Stack[Instr[3]]";
 	}
 };
 
 class vmut_concat_t : public vmutator_t {
 public:
 	vopcode_t* mutate( std::default_random_engine &rand_engine ) override {
-		return new vop_concat_t();
+		switch ( rand_engine() % 6 ) {
+			case 0: return new vop_concat_1_t();
+			case 1: return new vop_concat_2_t();
+			case 2: return new vop_concat_3_t();
+			case 3: return new vop_concat_bc_1_t();
+			case 4: return new vop_concat_bc_2_t();
+			case 5: return new vop_concat_bc_3_t();
+		}
+
+		return nullptr;
 	}
 };
 
