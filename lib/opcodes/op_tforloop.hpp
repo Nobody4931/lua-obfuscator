8acec6f92bf07a3724d1d678017a7872677d494c
Nobody4931 <ardenxie1@gmail.com>
2021-08-01 16:31:46 -0400

    op_tforloop

---
+++ lib/opcodes/op_tforloop.hpp
@@ -0,0 +1,68 @@
+#pragma once
+#ifndef OP_TFORLOOP_HEADER_HPP
+#define OP_TFORLOOP_HEADER_HPP
+
+#include <random>
+#include <string>
+
+#include "bytecode/instruction.hpp"
+#include "opcodes/vopcode.hpp"
+
+class vop_tforloop_1_t : public vopcode_t {
+public:
+	bool valid( instruction_t& instruction ) override {
+		return instruction.opcode == OP_TFORLOOP;
+	}
+
+	std::string string() override {
+		return R"(local A, C = Instr[1], Instr[3]
+
+		local Returns = { Stack[A]( Stack[A + 1], Stack[A + 2] ) }
+		for Idx = 1, C do
+			Stack[A + 2 + Idx] = Returns[Idx]
+		end
+
+		if Stack[A + 3] == nil then
+			InstrPtr = InstrPtr + 1
+		else
+			Stack[A + 2] = Stack[A + 3]
+		end)";
+	}
+};
+
+class vop_tforloop_2_t : public vopcode_t {
+public:
+	bool valid( instruction_t& instruction ) override {
+		return instruction.opcode == OP_TFORLOOP;
+	}
+
+	std::string string() override {
+		return R"(local A, Offset = Instr[1], Instr[1] + 2
+
+		local Returns = { Stack[A]( Stack[1 + A], Stack[A - -2] ) }
+		for Idx = 1, Instr[3] do
+			Stack[Idx + Offset] = Returns[Idx]
+		end
+
+		Offset = Stack[Offset + 1]
+		if Offset ~= nil then
+			Stack[2 - -A] = Offset
+		else
+			InstrPtr = 1 + InstrPtr
+		end)";
+	}
+};
+
+class vmut_tforloop_t : public vmutator_t {
+public:
+	vopcode_t* mutate( std::default_random_engine& rand_engine ) override {
+		switch ( rand_engine() % 2 ) {
+			case 0: return new vop_tforloop_1_t();
+			case 1: return new vop_tforloop_2_t();
+		}
+
+		return nullptr;
+	}
+};
+
+#endif
