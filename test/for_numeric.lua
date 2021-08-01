dac559fb5f9bc3dbf94bc8bf41aa0a3dcdd00b5b
Nobody4931 <ardenxie1@gmail.com>
2021-08-01 16:17:35 -0400

    make this garbage slightly less garbage

--- test/for_numeric.lua
+++ test/for_numeric.lua
@@ -2,6 +2,16 @@ local first = 0
 local last = 10
 local step = 0.2
 
+print("positive step")
+for iterator = first, last, step do
+	print(iterator)
+end
+
+first = 5
+last = -5
+step = -0.25
+
+print("negative step")
 for iterator = first, last, step do
 	print(iterator)
 end
